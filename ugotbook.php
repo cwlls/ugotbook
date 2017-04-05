3cdd1ed4ea6f644776e033e43061c170c4f85c9d
Gem Stone-Logan <gemstonelogan@gmail.com>
2017-04-04 21:03:41 -0400

    Everything working together

--- ugotbook.php
+++ ugotbook.php
@@ -1,46 +1,55 @@
-#!/usr/bin/env php
-<?php namespace ugotbook; 
+#!/usr/bin/php -q
+<?php namespace ugotbook;
 include_once('lib/functions.php');
 
 // Read in configuration file
 $config = parse_ini_file('config.ini', $process_sections = true);
 
-// API Configuration
+// gather pieces of configuration
 $endpoint = $config['API']['endpoint'];
 $client_key = $config['API']['client_key'];
 $client_secret = $config['API']['client_secret'];
-
-// DNA Configuration
 $dbhost = $config['DNA']['hostname'];
 $dbport = $config['DNA']['port'];
-$dbname = $config['DNA']['database_name'];
-$dbuser = $config['DNA']['database_user'];
-$dbpass = $config['DNA']['database_pass'];
+$dbname = $config['DNA']['dbase'];
+$dbuser = $config['DNA']['dbuser'];
+$dbpass = $config['DNA']['dbpass'];
 
 // create API object for requests
 $api = new API($endpoint, $client_key, $client_secret);
 
-// create a connection to the database
+// Change the 700 in the last line to 1 for daily orders
+$query = "select distinct v.field_content, b.title, rm.record_num
+    FROM sierra_view.bib_view b
+    JOIN sierra_view.bib_record_order_record_link bro ON bro.bib_record_id=b.id
+    JOIN sierra_view.order_view o ON o.id=bro.order_record_id
+    JOIN sierra_view.varfield v ON v.record_id=o.id AND varfield_type_code='n'
+    JOIN sierra_view.record_metadata rm ON rm.id=b.id AND rm.record_type_code='b'
+    WHERE
+    o.record_creation_date_gmt::date>=NOW()::DATE-EXTRACT(DOW FROM NOW())::INTEGER-700";
+
 $db = new DNA($dbhost, $dbport, $dbname, $dbuser, $dbpass);
+$result = $db ->query($query);
 
-// request patron using barcode
-$patron = $api->fetchPatron('1234567890123');
-print_r($patron);
+//Processes the SQL results. Make sure the config has the correct
+//barcode pattern for the library. 
+foreach($result as $line) {
+    if (preg_match_all($config['DNA']['bcode_pattern'],$line['field_content'],$note)) {
+        foreach($note as $barcodes){
+            //Accounts for multiple barcodes in note field
+            foreach ($barcodes as $barcode){
+                $bib_id = $line['record_num'];
+                $title = $line['title'];
+                // figures out patron ID
+                $patron=$api->fetchPatron($barcode);
+                //Attempts to make a request
+                $res=$api->requestHold($patron,$bib_id);
+                //Emails the patron
+                $res2 = $api->emailpatron($patron, $title);
 
-$holds = $api->fetchHolds($patron);
-print_r($holds);
 
-// request a hold
-$res = $api->requestHold($patron, '1000011');
-print_r($res);
+            }
+        }
+    }
+}
 
-// attempt to send email
-$res = $api->emailPatron($patron, '1000011');
-print_r($res);
-
-// request a list of 10 bibs via DNA
-// DOES NOT YET WORK
-// $bibs = $db->query("SELECT * from sierra_view.bibs_view LIMIT 10");
-// print_r($bibs);
-
-?>
--- ugotbooktest2.php
+++ ugotbooktest2.php
@@ -1,53 +1,46 @@
-#!/usr/bin/php -q
-<?php namespace ugotbook;
+#!/usr/bin/env php
+<?php namespace ugotbook; 
 include_once('lib/functions.php');
-
-//BEFORE EMAILING we need to double check we're not emailing
-//random people.
 
 // Read in configuration file
 $config = parse_ini_file('config.ini', $process_sections = true);
 
-// gather pieces of configuration
+// API Configuration
 $endpoint = $config['API']['endpoint'];
 $client_key = $config['API']['client_key'];
 $client_secret = $config['API']['client_secret'];
+
+// DNA Configuration
 $dbhost = $config['DNA']['hostname'];
 $dbport = $config['DNA']['port'];
-$dbname = $config['DNA']['dbase'];
-$dbuser = $config['DNA']['dbuser'];
-$dbpass = $config['DNA']['dbpass'];
+$dbname = $config['DNA']['database_name'];
+$dbuser = $config['DNA']['database_user'];
+$dbpass = $config['DNA']['database_pass'];
 
 // create API object for requests
 $api = new API($endpoint, $client_key, $client_secret);
 
-// Change the 700 in the last line to 1 for daily orders
-// Currently limiting to Phil's request
-$query = "select distinct v.field_content, b.title, rm.record_num
-    FROM sierra_view.bib_view b
-    JOIN sierra_view.bib_record_order_record_link bro ON bro.bib_record_id=b.id
-    JOIN sierra_view.order_view o ON o.id=bro.order_record_id
-    JOIN sierra_view.varfield v ON v.record_id=o.id AND varfield_type_code='n'
-    JOIN sierra_view.record_metadata rm ON rm.id=b.id AND rm.record_type_code='b'
-    WHERE
-    o.record_creation_date_gmt::date>=NOW()::DATE-EXTRACT(DOW FROM NOW())::INTEGER-700";
-
+// create a connection to the database
 $db = new DNA($dbhost, $dbport, $dbname, $dbuser, $dbpass);
-$result = $db ->query($query);
 
+// request patron using barcode
+$patron = $api->fetchPatron('1234567890123');
+print_r($patron);
 
-foreach($result as $line) {
-     if (preg_match_all($config['DNA']['bcode_pattern'],$line['field_content'],$note)) {
-        foreach($note as $barcodes){
-            //Accounts for multiple barcodes in note field
-            foreach ($barcodes as $barcode){
-                print "Barcode: " . $barcode . "\n";
-                $bib_id = $line['record_num'];
-                print "Bib ID: " . $bib_id . "\n";
-                $title = $line['title'];
-                print "Title: " . $title . "\n\n";
-            }
-        }
-    }
-}
+$holds = $api->fetchHolds($patron);
+print_r($holds);
 
+// request a hold
+$res = $api->requestHold($patron, '1000011');
+print_r($res);
+
+// attempt to send email
+$res = $api->emailPatron($patron, '1000011');
+print_r($res);
+
+// request a list of 10 bibs via DNA
+// DOES NOT YET WORK
+// $bibs = $db->query("SELECT * from sierra_view.bibs_view LIMIT 10");
+// print_r($bibs);
+
+?>
